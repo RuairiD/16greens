4e8e9a9df30b531ed35c7a0882bc19213f370ef7
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-08-26 18:24:17 -0700

    Controls, HUD, level changing, sound effects, water.

--- main.lua
+++ main.lua
@@ -67,15 +67,26 @@ local currentState
 
 -- START GAME
 
+-- START PHYSICS
 local WALL_TILES = {
     3, 4, 5,
     19, 21,
     35, 36, 37,
+    6, 7, 8,
+    22,
 }
 
-local function isWallTile(tile)
-    for _, wallTile in ipairs(WALL_TILES) do
-        if tile == wallTile then
+
+local WATER_TILES = {
+    48, 49, 50,
+    64, 65,
+    80, 81, 82,
+}
+
+
+local function isInTileset(tile, tileset)
+    for _, candidateTile in ipairs(tileset) do
+        if tile == candidateTile then
             return true
         end
     end
@@ -115,7 +126,7 @@ local function checkMove(x, y, newX, newY, width, height)
                 flr(xToCheck/8),
                 flr(y/8)
             )
-            if isWallTile(tile) then
+            if isInTileset(tile, WALL_TILES) then
                 resultX = xi - xStep
                 collidedX = true
                 break
@@ -139,7 +150,7 @@ local function checkMove(x, y, newX, newY, width, height)
                 flr(resultX/8),
                 flr(yToCheck/8)
             )
-            if isWallTile(tile) then
+            if isInTileset(tile, WALL_TILES) then
                 resultY = yi - yStep
                 collidedY = true
                 break
@@ -153,10 +164,37 @@ local function checkMove(x, y, newX, newY, width, height)
     return resultX, resultY, collidedX, collidedY
 end
 
+
+local function collides(x1, y1, w1, h1, x2, y2, w2, h2)
+    return (
+        x1 <= x2 + w2 and
+        x1 + w1 >= x2 and
+        y1 <= y2 + h2 and
+        y1 + h1 >= y2
+    )
+end
+
+
+local function containsPoint(px, py, x, y, w, h)
+    return (
+        px >= x and px <= x + w and
+        py >= y and py <= y + h
+    )
+end
+-- END PHYSICS
+
+local SFX = {
+    SPLASH = 63,
+    BOUNCE = 62,
+    STROKE = 61,
+    SINK = 60,
+    SELECT = 59,
+}
+
 local Ball = Object:extend()
 
-Ball.MAX_SPEED = 4
-Ball.FRICTION = 0.99
+Ball.MAX_SPEED = 8
+Ball.FRICTION = 0.98
 Ball.SIZE = 4
 
 function Ball:new(x, y)
@@ -167,6 +205,14 @@ function Ball:new(x, y)
     self.isStopped = true
 end
 
+function Ball:reset(x, y)
+    self.isStopped = true
+    self.velX = 0
+    self.velY = 0
+    self.x = x
+    self.y = y
+end
+
 function Ball:hit(angle, power)
     if not self.isStopped then
         return
@@ -174,6 +220,7 @@ function Ball:hit(angle, power)
     self.velX = power * Ball.MAX_SPEED * cos(angle)
     self.velY = power * Ball.MAX_SPEED * sin(angle)
     self.isStopped = false
+    sfx(SFX.STROKE)
 end
 
 function Ball:update()
@@ -202,6 +249,10 @@ function Ball:update()
             self.velY = 0
             self.isStopped = true
         end
+
+        if collidedX or collidedY then
+            sfx(SFX.BOUNCE)
+        end
     end
 end
 
@@ -212,25 +263,266 @@ function Ball:draw()
     spr(17, self.x - (8 - Ball.SIZE)/2, self.y - (8 - Ball.SIZE)/2)
 end
 
+-- Distance from ball where the line should start.
+local ANGLE_INDICATOR_BUFFER = 4
+local ANGLE_INDICATOR_LENGTH = 16
+local ANGLE_CHANGE_RATE = 0.005
+
+local POWER_BAR_WIDTH = 48
+local POWER_BAR_HEIGHT = 8
+local POWER_BAR_MARGIN = 1
+local POWER_BAR_X = 128 - POWER_BAR_WIDTH - POWER_BAR_MARGIN - 3
+local POWER_BAR_Y = POWER_BAR_MARGIN
+local POWER_CHANGE_RATE = 0.02
+
+local NEXT_HOLE_TIMER_MAX = 120
+local WATER_TIMER_MAX = 60
+
 local ball
+local ballOrigin
 local currentHole
+local scores
+local pin
+local angle
+local power
+local powerIncreasing
+local holeCompleted
+local nextHoleTimer
+local waterTimer
+local showingScore
+
+
+local function getHolePosition(holeNumber)
+    -- Overflow from final hole to first hole.
+    if holeNumber > 16 then
+        holeNumber = 1
+    end
+    local holeY = 0
+    local holeX = holeNumber - 1
+    if holeNumber > 8 then
+        holeY = 1
+        holeX = (16 - holeNumber)
+    end
+    return holeX, holeY
+end
+
+
+function initHole(holeNumber)
+    holeCompleted = false
+    showingScore = false
+    nextHoleTimer = 0
+    waterTimer = 0
+    scores[holeNumber] = 0
+
+    local holeX, holeY = getHolePosition(currentHole)
+
+    for x = holeX * 16, holeX * 16 + 16 do 
+        for y = holeY * 16, holeY * 16 + 16 do
+            local tile = mget(x, y)
+            if tile == 18 then
+                pin = {
+                    x = x * 8,
+                    y = y * 8,
+                }
+            elseif tile == 16 then
+                ballOrigin = {
+                    x = x * 8,
+                    y = y * 8,
+                }
+                ball:reset(x * 8 + 2, y * 8 + 2)
+            end
+        end
+    end
+end
+
 
 function initGame()
-    ball = Ball(32, 32) 
+    power = 0
+    powerIncreasing = true
+    angle = 0
+    ball = Ball(32, 32)
+    scores = {}
+    currentHole = 8
+    initHole(currentHole)
 end
 
 
 function updateGame()
-    if btnp(5) then
-        ball:hit(rnd(), 1)
+    if nextHoleTimer > 0 then
+        nextHoleTimer = nextHoleTimer - 1
+        if nextHoleTimer == 0 then
+            currentHole = currentHole + 1
+            initHole(currentHole)
+        end
     end
-    ball:update()
+
+    if waterTimer > 0 then
+        waterTimer = waterTimer - 1
+        if waterTimer == 0 then
+            ball:reset(
+                ballOrigin.x + 2,
+                ballOrigin.y + 2
+            )
+        end
+    end
+
+    if not holeCompleted and waterTimer == 0 then
+        if btn(0) then
+            angle = angle + ANGLE_CHANGE_RATE
+        elseif btn(1) then
+            angle = angle - ANGLE_CHANGE_RATE
+        end
+
+        if btn(5) then
+            if powerIncreasing then
+                power = power + POWER_CHANGE_RATE
+            else
+                power = power - POWER_CHANGE_RATE
+            end
+            if power > 1 then
+                power = 1
+                powerIncreasing = false
+            elseif power < 0 then
+                power = 0
+                powerIncreasing = true
+            end
+        elseif power > 0 then
+            ball:hit(angle, power)
+            power = 0
+            powerIncreasing = true
+
+            scores[currentHole] = scores[currentHole] + 1
+        end
+        ball:update()
+
+        if containsPoint(
+            ball.x + Ball.SIZE/2, ball.y + Ball.SIZE/2,
+            pin.x + 2, pin.y + 2, 4, 4
+        ) then
+            holeCompleted = true
+            showingScore = true
+            sfx(SFX.SINK)
+        end
+
+        -- Check for water. sploosh
+        local currentTile = mget(
+            flr((ball.x + Ball.SIZE/2)/8),
+            flr((ball.y + Ball.SIZE/2)/8)
+        )
+        if isInTileset(currentTile, WATER_TILES) then
+            waterTimer = WATER_TIMER_MAX
+            sfx(SFX.SPLASH)
+        end
+    else
+        if showingScore and btnp(5) then
+            showingScore = false
+            nextHoleTimer = NEXT_HOLE_TIMER_MAX
+            sfx(SFX.SELECT)
+        end
+    end
+end
+
+
+function drawHud()
+    -- Shadow
+    rectfill(
+        POWER_BAR_X, POWER_BAR_Y,
+        POWER_BAR_X + POWER_BAR_WIDTH + 2, POWER_BAR_Y + POWER_BAR_HEIGHT + 3,
+        5
+    )
+    -- White outline
+    rectfill(
+        POWER_BAR_X, POWER_BAR_Y,
+        POWER_BAR_X + POWER_BAR_WIDTH + 2, POWER_BAR_Y + POWER_BAR_HEIGHT + 2,
+        7
+    )
+    -- Red power bar
+    rectfill(
+        POWER_BAR_X + 1, POWER_BAR_Y + 1,
+        POWER_BAR_X + 1 + POWER_BAR_WIDTH * power, POWER_BAR_Y + 1 + POWER_BAR_HEIGHT,
+        8
+    )
+    -- Black unpowered section
+    rectfill(
+        POWER_BAR_X + 1 + POWER_BAR_WIDTH * power, POWER_BAR_Y + 1,
+        POWER_BAR_X + 1 + POWER_BAR_WIDTH, POWER_BAR_Y + 1 + POWER_BAR_HEIGHT,
+        0
+    )
+    -- Shadow text
+    print('power', POWER_BAR_X + 1 + (POWER_BAR_WIDTH - 20)/2, POWER_BAR_Y + 2 + (POWER_BAR_HEIGHT - 4)/2, 5)
+    -- Actual text
+    print('power', POWER_BAR_X + 1 + (POWER_BAR_WIDTH - 20)/2, POWER_BAR_Y + 1 + (POWER_BAR_HEIGHT - 4)/2, 7)
+
+    -- Hole Number
+    -- Shadow
+    rectfill(
+        POWER_BAR_MARGIN, POWER_BAR_MARGIN,
+        POWER_BAR_MARGIN + 32, 10 + POWER_BAR_MARGIN + 1,
+        5
+    )
+    -- White outline
+    rectfill(
+        POWER_BAR_MARGIN, POWER_BAR_MARGIN,
+        POWER_BAR_MARGIN + 32, 10 + POWER_BAR_MARGIN,
+        7
+    )
+    -- Black box
+    rectfill(
+        POWER_BAR_MARGIN + 1, POWER_BAR_MARGIN + 1,
+        POWER_BAR_MARGIN + 32 - 1, 10 + POWER_BAR_MARGIN - 1,
+        0
+    )
+    -- Hole number text
+    print(
+        'Hole '..tostring(currentHole),
+        POWER_BAR_MARGIN + 3,
+        POWER_BAR_MARGIN + 3,
+        7
+    )
+
+    -- Show score after completing hole
+    if showingScore then
+        rectfill(57, 57, 71, 71, 7)
+        rectfill(58, 58, 70, 70, 0)
+        print(scores[currentHole], 63, 62, 7)
+
+        rectfill(23, 85, 105, 95, 7)
+        rectfill(24, 86, 104, 94, 0)
+        print('press X to continue', (128 - 19 * 4)/2, 88, 7)
+    end
 end
 
 
 function drawGame()
-    map(0, 0, 0, 0, 16, 16)
-    ball:draw()
+    local holeX, holeY = getHolePosition(currentHole)
+
+    local cameraX = holeX * 128
+    local cameraY = holeY * 128
+    if holeCompleted and not showingScore then
+        local nextHoleX, nextHoleY = getHolePosition(currentHole + 1)
+        cameraX = holeX * 128 + (nextHoleX - holeX) * 128 * (1 - nextHoleTimer/NEXT_HOLE_TIMER_MAX)
+        cameraY = holeY * 128 + (nextHoleY - holeY) * 128 * (1 - nextHoleTimer/NEXT_HOLE_TIMER_MAX)
+    end
+
+    camera(cameraX, cameraY)
+    map(0, 0, 0, 0, 128, 32)
+
+    if not holeCompleted and waterTimer == 0 then
+        ball:draw()
+        if ball.isStopped then
+            line(
+                ball.x + Ball.SIZE/2 + ANGLE_INDICATOR_BUFFER * cos(angle),
+                ball.y + Ball.SIZE/2 + ANGLE_INDICATOR_BUFFER * sin(angle),
+                ball.x + Ball.SIZE/2 + (ANGLE_INDICATOR_BUFFER + ANGLE_INDICATOR_LENGTH) * cos(angle),
+                ball.y + Ball.SIZE/2 + (ANGLE_INDICATOR_BUFFER + ANGLE_INDICATOR_LENGTH) * sin(angle),
+                7
+            )
+        end
+    end
+
+    -- Hud drawing
+    camera(0, 0)
+    drawHud()
 end
 
 
